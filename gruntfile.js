5a137304eb4f127a7e20c1874076c435ae787c42
Max Bucknell <me@maxbucknell.com>
2014-04-06 17:31:31 +0100

    [change] gruntfile to use a variable

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,21 @@
 'use strict';
 
 module.exports = function (grunt) {
+  var config = { jshint: { options: { jshintrc: true }
+                         , dist: [ 'gruntfile.js'
+                                 , 'public/js/**/*.js'
+                                 , 'app.js'
+                                 , 'helpers/**/*.js'
+                                 , 'routes/**/*.js'
+                                 ]
+                         }
+               , sass: { options: { style: 'expanded' }
+                       , dist: { files: { 'public/css/main.css': 'public/sass/main.sass' }}
+                       }
+               }
 
-  grunt.initConfig({ jshint: { options: { jshintrc: true }
-														 , dist: [ 'gruntfile.js'
-																	 	, 'public/js/**/*.js'
-                                     , 'app.js'
-                                     , 'helpers/**/*.js'
-																	 	, 'routes/**/*.js'
-																	 	]
-    												 }
-									 , sass: { options: { style: 'expanded' }
-													 , dist: { files: { 'public/css/main.css': 'public/sass/main.sass' }
-													 }
-									 }
-	})
+
+  grunt.initConfig(config)
 
   grunt.loadNpmTasks('grunt-contrib-jshint')
 	grunt.loadNpmTasks('grunt-contrib-sass')
